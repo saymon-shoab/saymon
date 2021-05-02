3bb794b713c4c775ca12eb8972a298990e189f40
saymon-shoab <mdsaymonshoab@gmail.com>
2021-05-02 21:55:26 +0600

    color changed

--- src/Components/Home/Banner/Banner.js
+++ src/Components/Home/Banner/Banner.js
@@ -7,7 +7,7 @@ const Banner = () => {
             <div className="container">
                 <div className="row">
                     <div style={{color:'black'}} className="col-lg-6 col-sm-12 text-center m-auto">
-                    <h3 className="">Hi, I am Saymon Shoab</h3>
+                    <h3 >Hi, I am Saymon Shoab</h3>
                     <h4>WEB DEVELOPER</h4>
                     <div class="header-button">
                     <a href="https://drive.google.com/file/d/1n7lsCzccyaMDqK5WQMmG3L-jfKzRVi-1/view?usp=sharing" target="blank">resume</a>
